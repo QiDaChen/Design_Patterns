0bca5dae2b8baa16cad2ca4b8d4619819b60b71c
yanhuijin <wb.yanhuijin@mesg.corp.netease.com>
2020-02-15 16:47:44 +0800

    add listen_mode

--- mode_listening.py
+++ mode_listening.py
@@ -5,3 +5,12 @@
 # Author: chen.qi
 # Mail:chenphoun#outlook.com
 # createtime: 2020/2/15 0:14
+
+class HotWaterListen():
+    def __init__(self):
+        self.m_lineTemp = 20
+        self.m_lineTime = 10
+        self.m_temp = 0
+        self.upTemp()
+    def upTemp(self):
+        self.m_tem+=1 
